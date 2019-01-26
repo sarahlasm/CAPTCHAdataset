b27d2ced51b2bda02f3638bc858c032695e5df16
Sarah Lasman <lasmansarah@gmail.com>
2019-01-25 23:45:38 -0800

    Added comments to CAPTCHA_CNN.py

--- CAPTCHA_CNN.py
+++ CAPTCHA_CNN.py
@@ -9,6 +9,7 @@ import cv2
 num_pics = 20262
 symbols = string.ascii_lowercase + "0123456789"
 
+# Pre-process the (already cropped) data
 def process_data():
     X = np.zeros((num_pics, 15, 30, 1))
     y = np.zeros((1, num_pics, 36))
@@ -19,10 +20,13 @@ def process_data():
             break
         if not (address.endswith('.jpg')):
             continue
+        # Read as greyscale, resize
         img = cv2.imread((os.path.join('./Cropped', pic)), cv2.IMREAD_GRAYSCALE)
         img = cv2.resize(img, (30, 15))
+        # Store image array in X
         X[count, :, :, 0] = img
         targs = np.zeros((1, 36))
+        # Determine the solution and add to the y index
         index = symbols.find(address[address.find("_") + 1])
         targs[0,index] = 1
         y[:, count] = targs
@@ -30,6 +34,7 @@ def process_data():
     # Return final data
     return X, y
     
+# Create the CNN    
 def CNN():
     model = Sequential()
     model.add(Conv2D(64, kernel_size=3, activation='relu', input_shape=(15,30,1)))
@@ -52,6 +57,7 @@ history = cnn.fit(X_train, [y_train[0]], batch_size = 64, validation_data=(X_tes
 THRESHOLD = 165
 LUT = [0]*THRESHOLD + [1]*(256 - THRESHOLD)
 
+# Modified crop method from Crop.py file
 def capt_process(capt):
     capt_gray = capt.convert("L")
     capt_bw = capt_gray.point(LUT, "1")
@@ -65,6 +71,9 @@ def capt_process(capt):
 
     return capt_per_char_list
 
+# Predict all four characters in a CAPTCHA
+# Param address - the file name of the image
+# Returns the solved CAPTCHA
 def predict4(address):
     im = Image.open(os.path.join('./Final Train Data', address))
     im = im.resize((60,33),Image.ANTIALIAS)
@@ -79,6 +88,7 @@ def predict4(address):
         answer += i
     return answer
 
+# Processes each character individually 
 def predict(address):
     img = cv2.imread((os.path.join('./Cropped', address)), cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (30, 15))
